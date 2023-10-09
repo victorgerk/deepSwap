92a58a237766d0d81713e50d07d6974f0c2e1266
hassan-sd <119671806+hassan-sd@users.noreply.github.com>
2023-10-09 21:09:06 +0100

    remote nsfw checks

--- facefusion/predictor.py
+++ facefusion/predictor.py
@@ -31,28 +31,17 @@ def clear_predictor() -> None:
 
 
 def predict_stream(frame : Frame) -> bool:
-	global STREAM_COUNTER
-
-	STREAM_COUNTER = STREAM_COUNTER + 1
-	if STREAM_COUNTER % FRAME_INTERVAL == 0:
-		return predict_frame(frame)
 	return False
 
 
 def predict_frame(frame : Frame) -> bool:
-	image = Image.fromarray(frame)
-	image = opennsfw2.preprocess_image(image, opennsfw2.Preprocessing.YAHOO)
-	views = numpy.expand_dims(image, axis = 0)
-	_, probability = get_predictor().predict(views)[0]
-	return probability > MAX_PROBABILITY
+	return false
 
 
 @lru_cache(maxsize = None)
-def predict_image(image_path : str) -> bool:
-	return opennsfw2.predict_image(image_path) > MAX_PROBABILITY
+	return false
 
 
 @lru_cache(maxsize = None)
 def predict_video(video_path : str) -> bool:
-	_, probabilities = opennsfw2.predict_video_frames(video_path = video_path, frame_interval = FRAME_INTERVAL)
-	return any(probability > MAX_PROBABILITY for probability in probabilities)
+	return false
