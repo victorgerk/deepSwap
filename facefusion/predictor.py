ec08dd3ffa98cb78066b0a7d81733aec02957816
hassan-sd <119671806+hassan-sd@users.noreply.github.com>
2023-10-10 22:38:55 +0100

    Update predictor.py

--- facefusion/predictor.py
+++ facefusion/predictor.py
@@ -9,39 +9,37 @@ from keras import Model
 from facefusion.typing import Frame
 
 PREDICTOR = None
-THREAD_LOCK : threading.Lock = threading.Lock()
+THREAD_LOCK: threading.Lock = threading.Lock()
 MAX_PROBABILITY = 0.75
 FRAME_INTERVAL = 25
 STREAM_COUNTER = 0
 
 
 def get_predictor() -> Model:
-	global PREDICTOR
+    global PREDICTOR
 
-	with THREAD_LOCK:
-		if PREDICTOR is None:
-			PREDICTOR = opennsfw2.make_open_nsfw_model()
-	return PREDICTOR
+    with THREAD_LOCK:
+        if PREDICTOR is None:
+            PREDICTOR = opennsfw2.make_open_nsfw_model()
+    return PREDICTOR
 
 
 def clear_predictor() -> None:
-	global PREDICTOR
+    global PREDICTOR
 
-	PREDICTOR = None
-
-
-def predict_stream(frame : Frame) -> bool:
-	return False
+    PREDICTOR = None
 
 
-def predict_frame(frame : Frame) -> bool:
-	return false
+def predict_stream(frame: Frame) -> bool:
+    # do nothing
+    return False
 
 
-@lru_cache(maxsize = None)
-	return false
+def predict_frame(frame: Frame) -> bool:
+    # do nothing
+    return False
 
 
-@lru_cache(maxsize = None)
-def predict_video(video_path : str) -> bool:
-	return false
+@lru_cache(maxsize=None)
+def predict_video(video_path: str) -> bool:
+    return False
