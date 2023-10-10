50613d221e3b429e29ef5d1adf978995d2d6082f
hassan-sd <119671806+hassan-sd@users.noreply.github.com>
2023-10-10 23:13:14 +0100

    Update predictor.py

--- facefusion/predictor.py
+++ facefusion/predictor.py
@@ -9,43 +9,44 @@ from keras import Model
 from facefusion.typing import Frame
 
 PREDICTOR = None
-THREAD_LOCK : threading.Lock = threading.Lock()
+THREAD_LOCK: threading.Lock = threading.Lock()
 MAX_PROBABILITY = 0.75
 FRAME_INTERVAL = 25
 STREAM_COUNTER = 0
 
 
 def get_predictor() -> Model:
-	global PREDICTOR
+    global PREDICTOR
 
-	with THREAD_LOCK:
-		if PREDICTOR is None:
-			PREDICTOR = opennsfw2.make_open_nsfw_model()
-	return PREDICTOR
+    with THREAD_LOCK:
+        if PREDICTOR is None:
+            PREDICTOR = opennsfw2.make_open_nsfw_model()
+    return PREDICTOR
 
 
 def clear_predictor() -> None:
-	global PREDICTOR
+    global PREDICTOR
 
-	PREDICTOR = None
-
-
-def predict_stream(frame : Frame) -> bool:
-
-	return False
+    PREDICTOR = None
 
 
-def predict_frame(frame : Frame) -> bool:
-
-	return False
-
-
-@lru_cache(maxsize = None)
-def predict_image(image_path : str) -> bool:
-	return opennsfw2.predict_image(image_path) > MAX_PROBABILITY
+def predict_stream(frame: Frame) -> bool:
+    # NSFW check disabled, always return False.
+    return False
 
 
-@lru_cache(maxsize = None)
-def predict_video(video_path : str) -> bool:
-	_, probabilities = opennsfw2.predict_video_frames(video_path = video_path, frame_interval = FRAME_INTERVAL)
-	return any(probability > MAX_PROBABILITY for probability in probabilities)
+def predict_frame(frame: Frame) -> bool:
+    # NSFW check disabled, always return False.
+    return False
+
+
+@lru_cache(maxsize=None)
+def predict_image(image_path: str) -> bool:
+    # NSFW check disabled, always return False.
+    return False
+
+
+@lru_cache(maxsize=None)
+def predict_video(video_path: str) -> bool:
+    # NSFW check disabled, always return False.
+    return False
